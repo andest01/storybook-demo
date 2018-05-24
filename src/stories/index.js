1917ecc66e117cc52bc5af95347cdff0f3e83e9e
Stu Anderson <andest01@gmail.com>
2018-05-24 18:47:27 -0500

    initial commit

---
+++ src/stories/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+
+const myListOfStories = storiesOf('Welcome', module)
+myListOfStories.addDecorator(withKnobs);
+
+myListOfStories.add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+
+  myListOfStories.add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => {
+    const myText = text('textLabel', 'asdfasf')
+    return (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        {myText}
+      </span> 
+    </Button>
+  )
+  });
